29c0b58fef805d82cf5985530066e3ef454ab22b
EstephaniaCalvoC <2177@holbertonschool.com>
2021-03-09 14:39:36 -0500

    Fix betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,14 +8,14 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
-	
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new)
 	{
 		new->n = value;
 		new->parent = parent;
 		new->left = new->right = NULL;
-		return new;
+		return (new);
 	}
-	return NULL;
+	return (NULL);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,9 +13,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	
+
 	full = binary_tree_is_full(tree->left) &&
 		binary_tree_is_full(tree->right);
-	
+
 	return (full);
 }
