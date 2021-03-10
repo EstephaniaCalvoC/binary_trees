2c8818bd9e43159f272b483166c40fab27ea503d
EstephaniaCalvoC <2177@holbertonschool.com>
2021-03-10 14:05:00 -0500

    fix betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,6 +33,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left = tree->left ? binary_tree_height(tree->left) : -1;
-	right = tree->right ? binary_tree_height(tree->right): -1;
+	right = tree->right ? binary_tree_height(tree->right) : -1;
 	return (left - right);
 }
